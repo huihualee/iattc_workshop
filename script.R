4a43e2d87feba4df9e4636c1101afa990241a0c3
huihualee <huihualee@gmail.com>
2019-02-14 11:38:26 -0800

    first message

---
+++ script.R
@@ -0,0 +1,7 @@
+# Install and load devtools package
+install.packages("devtools")
+library("devtools")
+
+install_github("hoyles/cpue.rfmo", username = "huihualee", auth_token = "d7a74b67ec4f0a2dec7a3d1dedcdd050f3a5a60c")
+library(cpue.rfmo)
+
