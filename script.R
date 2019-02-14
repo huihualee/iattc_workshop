07c366dc16954ab102e6807696a07ca4e4c33d31
huihualee <huihualee@gmail.com>
2019-02-14 11:44:59 -0800

    add plot

--- script.R
+++ script.R
@@ -5,3 +5,5 @@ library("devtools")
 install_github("hoyles/cpue.rfmo", username = "huihualee", auth_token = "d7a74b67ec4f0a2dec7a3d1dedcdd050f3a5a60c")
 library(cpue.rfmo)
 
+#making a plot
+#plot()
